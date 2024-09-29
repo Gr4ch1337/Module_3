42e66817c65c116da3a9469a221d0e1a7328bae5
Gr4ch1337 <sneG4ee@mail.ru>
2024-09-29 14:01:02 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params('строка', 1, False)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = ['список' , 1 , True]
+values_dict = {'a' : 'словарь', 'b' : 2 , 'c' : False}
+
+print_params(*values_list)
+print_params(**values_dict)
