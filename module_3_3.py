4ba79b20f9ab67e6d98cd1cb9b7657caa526ca24
Gr4ch1337 <sneG4ee@mail.ru>
2024-09-30 13:38:03 +0300

    module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,13 +1,17 @@
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
+
 
 print_params()
 print_params('строка', 1, False)
-print_params(b = 25)
-print_params(c = [1,2,3])
+print_params(b=25)
+print_params(c=[1, 2, 3])
 
-values_list = ['список' , 1 , True]
-values_dict = {'a' : 'словарь', 'b' : 2 , 'c' : False}
+values_list = ['список', 1, True]
+values_dict = {'a': 'словарь', 'b': 2, 'c': False}
+
+values_list_2 = [1123, 1.5]
 
 print_params(*values_list)
 print_params(**values_dict)
+print_params(*values_list_2, 42)
