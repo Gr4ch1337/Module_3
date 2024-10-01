15ff70aae4a7149ba56177035c8f64a527050ceb
Gr4ch1337 <sneG4ee@mail.ru>
2024-10-01 17:58:26 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,13 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if first == 0:
+        return first
+    elif len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+result = get_multiplied_digits(1023)
+print(result)
