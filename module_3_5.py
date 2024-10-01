e21e871388f8f8184f6899efadf956b68a31edfe
Gr4ch1337 <sneG4ee@mail.ru>
2024-10-01 20:40:17 +0300

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,13 +1,15 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    for i in str_number:
+        if i == '0':
+            str_number = str_number.replace('0', '')
+            break
     first = int(str_number[0])
-    if first == 0:
-        return first
-    elif len(str_number) > 1:
+    if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
 
-result = get_multiplied_digits(1023)
+result = get_multiplied_digits('00000102400000')
 print(result)
